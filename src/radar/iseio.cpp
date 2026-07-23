42eaa613eb281c4c3e37f38a11a82c5edfd31b8d
Artem Setov <setov@iszf.irk.ru>
2026-07-23 14:37:16 +0800

    WIP ISE reader (header reader from files and multicast)

--- src/radar/iseio.cpp
+++ src/radar/iseio.cpp
@@ -1,10 +1,23 @@
+#include <complex>
+#include <cstddef>
+#include <cstdint>
+#include <ios>
+#include <iosfwd>
+#include <optional>
 #include <string_view>
 #include <stdexcept>
-
-using namespace std;
+#include <fstream>
+#include <unordered_map>
+#include <vector>
+#include <array>
+#include <span>
 
 
 namespace ise {
+
+constexpr std::array<std::byte, 4> KEYWORD = {
+    std::byte{'O'}, std::byte{'R'}, std::byte{'D'}, std::byte{'A'}
+};
 
 enum class ParameterCode {
     reserved = 0,     // reserved
@@ -74,38 +87,159 @@ enum class ErrorPolicy {
 
 class ErrorHandler {
 public:
-    ErrorHandler(ErrorPolicy policy) : policy_(policy) {}
+    ErrorHandler(ErrorPolicy policy) : m_policy(policy) {}
 
-    void handle_error(string_view msg) {
-        if (policy_ == ErrorPolicy::kRaise) {
-            throw runtime_error(msg);
-        } else if (policy_ == ErrorPolicy::kWarn) {
-            cout << msg << endl;
+    void handle_error(std::string_view msg) {
+        if (m_policy == ErrorPolicy::kRaise) {
+            throw std::runtime_error(msg);
+        } else if (m_policy == ErrorPolicy::kWarn) {
+            std::cerr << msg << std::endl;
         } else {
-            throw invalid_argument("Unexpected error policy");
+            throw std::invalid_argument("Unexpected error policy");
         }
     }
 
 private:
-    ErrorPolicy policy_;
+    ErrorPolicy m_policy;
+};
+
+
+struct ParamsHeaderValue {
+    std::unordered_map<ParameterCode, uint16_t> params;
+};
+
+struct DataHeaderValue {
+    std::vector<std::complex<float>> data;
+};
+
+
+class Header {
+    HeaderCode m_code;
+    size_t m_length;
+    std::variant<ParamsHeaderValue, DataHeaderValue> m_value;
+};
+
+
+class AbstractIseStream {
+public:
+    virtual ~AbstractIseStream() {}
+
+    // Read the next header
+    virtual std::optional<Header> read_header() = 0;
+};
+
+
+class IseFileStream : public AbstractIseStream {
+public:
+    IseFileStream(std::span<std::string_view> paths, ErrorPolicy policy)
+    : m_paths(paths)
+    , m_policy(policy) {
+        read_next_file();
+    }
+
+    std::optional<Header> read_header() override {
+        while (true) {
+            advance_to_keyword();
+            if (m_pos >= m_size && !read_next_file()) {
+                // No more files to read
+                return std::nullopt;
+            }
+        }
+    }
+
+private:
+    std::span<std::string_view> m_paths;
+    ErrorPolicy m_policy;
+    size_t m_path_idx = 0;
+    std::ifstream m_ifstream;
+    size_t m_size = 0;
+    size_t m_pos = 0;
+    std::vector<std::byte> m_buffer;
+
+    void advance_to_keyword() {
+        // Search for the keyword in the buffer
+        auto result = std::ranges::search(m_buffer.begin() + m_pos, m_buffer.end(),
+                                          KEYWORD.begin(), KEYWORD.end());
+        if (result != m_buffer.end()) {
+            m_pos = result.begin() - m_buffer.begin() + KEYWORD.size();
+        } else {
+            m_pos = m_size;
+        }
+    }
+
+    std::span<const std::byte> read(size_t n_bytes) override {
+        // Check if we've reached the end of the file
+        if ((m_pos + n_bytes) >= m_size) {
+            return std::span<const std::byte>();
+        }
+
+        // Search for the keyword in the buffer
+        auto result = std::ranges::search(m_buffer, KEYWORD);
+        if (result == m_buffer.end()) {
+            m_pos = m_size;
+            return std::span<const std::byte>();
+        }
+
+        // Advance the position to the end of the keyword
+        m_pos = static_cast<std::streampos>(result - m_buffer.begin() + KEYWORD.size());
+        return std::span<const std::byte>(m_buffer);
+    }
+
+    bool read_next_file() {
+        if (m_path_idx >= m_paths.size()) {
+            return false;
+        }
+        if (m_path_idx != 0) {
+            m_ifstream.close();
+        }
+        m_ifstream.open(m_paths[m_path_idx], std::ios::binary | std::ios::ate);
+        m_size = static_cast<size_t>(m_ifstream.tellg());
+        m_ifstream.seekg(0);
+        m_buffer.resize(m_size);
+        m_ifstream.read(reinterpret_cast<char*>(m_buffer.data()),
+                        static_cast<std::streamsize>(m_size));
+        m_path_idx++;
+        return true;
+    }
+};
+
+
+class IseMulticastStream : public AbstractIseStream {
+public:
+    IseMulticastStream(MulticastParams params) : m_params(params) {}
+
+    std::span<const uint8_t> advanced_read() override {
+
+    }
+
+private:
+    MulticastParams m_params;
 };
 
 
 class IseHeaderReader {
 public:
-    IseHeaderReader(IseIOStream stream, bool conjugate_quadratures = true, 
-        ErrorPolicy error_policy = ErrorPolicy::kRaise)
-        : stream_(stream)
-        , conjugate_quadratures_(conjugate_quadratures)
-        , error_handler_(ErrorHandler(error_policy))
+    IseHeaderReader(IseIOStream stream, ErrorPolicy error_policy = ErrorPolicy::kRaise)
+        : m_stream(stream)
+        , m_error_handler(ErrorHandler(error_policy))
     {}
 
-
+    Header read_header() {
+        while (true) {
+            auto buffer = m_stream.read_chunk(9);
+            if (buffer.size() != 9) {
+                throw std::runtime_error("Header size mismatch");
+            }
+            auto result = std::ranges::search(buffer, KEYWORD);
+            if (result != buffer.end()) {
+                return Header();
+            }
+        }
+    }
 
 private:
-    IseIOStream stream_;
-    bool conjugate_quadratures_;
-    ErrorHandler error_handler_;
+    IseIOStream m_stream;
+    ErrorHandler m_error_handler;
 };
 
 }  // namespace
